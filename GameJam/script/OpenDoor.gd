01b796a30a151e77649f279115142884706785d9
Taedriel <isam.delorme@gmail.com>
2020-12-12 10:29:11 +0100

    door ok

--- GameJam/script/OpenDoor.gd
+++ GameJam/script/OpenDoor.gd
@@ -1,19 +1,37 @@
 extends Node2D
 
+var cible = null
+var inZone = false setget door
+var dest = null
+var cpt = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var children = get_children()
+	#on cherche dans les enfants
+	for child in get_children():
+		if child.has_method("door"):
+			dest = child.get_global_position()
+			return
+			
+	#on cherche dans les parents
+	dest = get_parent().get_global_position()
+	return
+
+func door(_door):
+	return inZone
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event.is_action_pressed('ui_select') and inZone and cible != null:
+		cible.position.x = dest.x
+		cible.position.y = dest.y
 
 
-func _on_Area2D_area_entered(area):
-	if Input.is_action_pressed('ui_up'):
-		for p in get_tree().get_nodes_in_group("player"):
-			p.position.x += 10
-			p.position.y += 10 
+func _on_Area2D_body_entered(body):
+	cible = body
+	inZone = true
 	
 
+func _on_Area2D_body_exited(body):
+	inZone = false
+	cible = null
