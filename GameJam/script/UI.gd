a28eab2ad9e80162595f57ecf51b4e1859e7402d
ln-dev05 <lord.nightmare05@gmail.com>
2020-12-12 20:57:08 +0100

    Modifications qui seront validées : 	modifié :         GameJam/godot_component/Tilemap/terrain.tscn 	nouveau fichier : GameJam/godot_component/scene/GameMenu.tscn 	modifié :         GameJam/godot_component/scene/Menu.tscn 	modifié :         GameJam/godot_component/scene/UI.tscn 	nouveau fichier : GameJam/media/img/logo/Logo.png 	nouveau fichier : GameJam/media/img/logo/Logo.png.import 	nouveau fichier : GameJam/media/img/logo/Patrick.png 	nouveau fichier : GameJam/media/img/logo/Patrick.png.import 	nouveau fichier : GameJam/media/img/logo/PatrickRouge.png 	nouveau fichier : GameJam/media/img/logo/PatrickRouge.png.import 	nouveau fichier : GameJam/media/img/logo/Test.png 	nouveau fichier : GameJam/media/img/logo/Test.png.import 	nouveau fichier : GameJam/media/img/logo/projet_logo.png 	nouveau fichier : GameJam/media/img/logo/projet_logo.png.import 	modifié :         GameJam/project.godot 	modifié :         GameJam/script/UI.gd

--- GameJam/script/UI.gd
+++ GameJam/script/UI.gd
@@ -1,4 +1,16 @@
 extends Control
 	
+var menu = preload("res://godot_component/scene/GameMenu.tscn")
+	
+func get_input():
+	var menu_instance = menu.instance()
+	if Input.is_action_just_pressed("ui_home"):
+		get_tree().change_scene("res://godot_component/scene/Menu.tscn")
+	
+func _process(delta):
+	get_input()
+	
+
+	
 func _on_next_second(newSec):
 	get_tree().get_nodes_in_group("chrono")[0].text = str(newSec)
