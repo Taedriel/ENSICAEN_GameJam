5e41f807ef745220770a34d35a04017fff2433b9
Taedriel <isam.delorme@gmail.com>
2020-12-13 09:15:17 +0100

    trotard

--- GameJam/script/Switch.gd
+++ GameJam/script/Switch.gd
@@ -2,15 +2,23 @@ extends StaticBody2D
 
 var is_activate = false
 export (bool) var left = true
+var inZone = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.flip_h = !left
-	pass # Replace with function body.
 
-func _on_Area2D_body_entered(body):
-	if body.name == "RockBullet":
+
+func _input(event):
+	
+	if event.is_action_pressed('ui_select') and inZone:
 		is_activate = !is_activate
 		$Sprite.flip_h = !$Sprite.flip_h
 		$AudioStreamPlayer2D.play()
 
+
+func _on_Area2D_body_entered(body):
+	inZone = true
+
+func _on_Area2D_body_exited(body):
+	inZone = false
